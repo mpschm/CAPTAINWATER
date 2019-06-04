5b1354769dab09bd532b29a68bbcd64ca8c74b29
Daniela Azevedo <azevedod@gmail.com>
2019-06-04 18:28:23 +0200

    dependent-destroy-seed

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,8 @@
 class Game < ApplicationRecord
   belongs_to :user
 
-  has_many :questions
-  has_many :plays
+  has_many :questions, dependent: :destroy
+  has_many :plays, dependent: :destroy
   # has_many :users, through: :plays
   has_many :players, through: :plays, class_name: "User", source: :user
 end
