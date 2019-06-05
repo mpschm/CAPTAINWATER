241aa2c92b1e8400584285ba5bc83c7980051d0e
Marie Estelle IOrio <marieestelle.iorio@gmail.com>
2019-06-05 19:38:42 +0200

    modifié

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,28 @@
 class GamesController < ApplicationController
   def new
     authorize @game
+    @game = Game.new
   end
 
   def create
     authorize @game
+    @game = Game.new(game_params)
+    @game.user = current_user
+    if @game.save
+      redirect_to play_path(@play)
+    else
+      render :new
+    end
   end
 
   def launch
     authorize current_user
   end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :code, :current_step, :user_id)
+  end
+
 end
