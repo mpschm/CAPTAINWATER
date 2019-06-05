791587f99c0a7a7f22a05beeb84f85db4d358a40
Daniela Azevedo <azevedod@gmail.com>
2019-06-05 16:59:36 +0200

    plays_user-answers_games_controller-plays-show

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,13 +1,34 @@
 class PlaysController < ApplicationController
   def show
-    authorize current_user
+    @play = Play.find(params[:id])
+    authorize @play
+    @game = @play.game
+    @question = @game.questions.find_by(step: @game.current_step)
+    @answers = @question.answers
+    @user_answer = UserAnswer.new
   end
 
-  def new
-    authorize current_user
-  end
+  # def new
+  #   @play = Play.new(play_params)
+  #     if @Play.save
+  #       redirect_to
+  #   # authorize current_user
+  # end
 
   def create
+    #@play = TODO
     authorize @play
+    redirect_to play_path(@play)
   end
 end
+
+# Timer
+
+# def countdown(seconds)
+#   date1 = Time.now + seconds
+#   while Time.now < date1
+#     t = Time.at(date1.to_i - Time.now.to_i)
+#     puts t.strftime('%M:%S')
+#     sleep 1
+#   end
+# end
