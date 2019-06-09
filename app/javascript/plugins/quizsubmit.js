c8a91d8b73546265e1e587bba238b51e8fcecd52
Marie Estelle IOrio <marieestelle.iorio@gmail.com>
2019-06-09 22:25:03 +0200

    modifié

--- app/javascript/plugins/quizsubmit.js
+++ app/javascript/plugins/quizsubmit.js
@@ -1,3 +1,14 @@
-document.querySelector("d-none").onclick = function() {
-  document.getElementById("submit-answer").click();
-};
+
+// document.querySelectorAll("label").forEach((choice) => {
+//   choice.addEventListener("click", (event) => {
+//     document.getElementById("submit-answer").click();
+//   });
+// });
+
+var choices = document.querySelectorAll("label");
+// Listen for click events on body
+document.body.addEventListener('click', function (event) {
+    if (choices.contains(event.target)) {
+        document.getElementById("submit-answer").click();
+    }
+});
