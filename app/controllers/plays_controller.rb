3afed5c3e1dc83704a6254e9ad298c71e8fb37a3
Daniela Azevedo <azevedod@gmail.com>
2019-06-10 10:42:53 +0200

    bubbles-ok

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -29,7 +29,7 @@ class PlaysController < ApplicationController
 
   def index
     @game = Game.find(params[:game_id])
-    @plays = policy_scope(Play).where(game: @game)
+    @play = policy_scope(Play).where(game: @game)
     @result = current_user.plays.find_by(game: @game).score
   end
 
